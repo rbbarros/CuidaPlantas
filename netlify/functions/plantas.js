1ffb1de17e3b67487822b5d8b6f5b0338f0d1cf2
rbbarros <raquelbarros.agro@gmail.com>
2026-08-25 19:05:16 -0300

    feat: create CuidaPlanta web app

---
+++ netlify/functions/plantas.js
@@ -0,0 +1,15 @@
+exports.handler = async function() {
+    const chave = process.env.PERENUAL_API_KEY;
+
+    const resposta = await fetch(
+        `https://www.perenual.com/api/v2/species-list?key=${chave}&indoor=1`
+    );
+
+    const dados = await resposta.json();
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dados)
+    };
+};
