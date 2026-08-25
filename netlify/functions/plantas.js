744766d78177b61fb0a3f9a8a13427ffc5c62c22
rbbarros <raquelbarros.agro@gmail.com>
2026-08-25 20:24:09 -0300

    feat: finalize plant care application

--- netlify/functions/plantas.js
+++ netlify/functions/plantas.js
@@ -1,10 +1,6 @@
 exports.handler = async function() {
     const chave = process.env.PERENUAL_API_KEY;
-
-    const resposta = await fetch(
-        `https://www.perenual.com/api/v2/species-list?key=${chave}&indoor=1&watering=average&sunlight=part_shade`
-    );
-
+    const resposta = await fetch(`https://www.perenual.com/api/v2/species-list?key=${chave}&indoor=1`);
     const dados = await resposta.json();
 
     return {
